10f107b28cfadde8ec3dcae92cbf0119f6107eb8
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-08-21 22:05:57 +0800

    cms: register & login function

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
