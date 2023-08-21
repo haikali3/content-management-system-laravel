f2ac076ec76feb62d6385484593fcdc864332825
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-08-22 01:50:04 +0800

    +attribute for post&create table +side bar nav

--- database/migrations/2023_08_21_164452_create_posts_table.php
+++ database/migrations/2023_08_21_164452_create_posts_table.php
@@ -4,24 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void {
+    Schema::create('posts', function (Blueprint $table) {
+      $table->id();
+      $table->string('title');
+      $table->string('description');
+      $table->text('content');
+      $table->string('image');
+      $table->timestamp('published_at')->nullable();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('posts');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void {
+    Schema::dropIfExists('posts');
+  }
 };
