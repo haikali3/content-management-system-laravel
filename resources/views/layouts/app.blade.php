de4b0204133086653bac4154ab2225bca1a3f1b5
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-08-26 14:48:02 +0800

    create post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
                         <div class="col-md-4">
                             <ul class="list-group">
                                 <li class="list-group-item">
-                                    <a href="/post">Posts</a>
+                                    <a href="{{ route('posts.index') }}">Posts</a>
                                 </li>
                                 <li class="list-group-item">
                                     <a href="{{ route('categories.index') }}">Categories</a>
