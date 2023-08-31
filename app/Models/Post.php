bf17a5c21e937d9b5715f6e3370fdf8c908e90ab
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-08-31 13:39:41 +0800

    soft delete model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model {
+  use SoftDeletes;
+
   protected $fillable = ['title', 'description', 'content', 'image', 'published_at'];
 }
