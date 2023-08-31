bf38cfd2aa620bded866568a5ff6b0c5be76fc64
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-08-31 12:42:44 +0800

    display image on posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,36 @@
 <div class="card card-default">
     <div class="card-header">Posts</div>
     <div class="card-body">
+        <table class="table">
+            <thead>
+                <th>Image</th>
+                <th>Title</th>
+                <th></th>
+                <th></th>
 
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                <tr>
+                    <td>
+                        <img src="{{ asset( $post->image) }}" width="120px" height="60px" alt="">
+                    </td>
+                    <td>
+                        {{ $post->image }}
+                    </td>
+                    <td>
+                        {{ $post->title}}
+                    </td>
+                    <td>
+                        <a href="" class="btn btn-info btn-sm">Edit</a>
+                    </td>
+                    <td>
+                        <a href="" class="btn btn-danger btn-sm">Trash</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 
