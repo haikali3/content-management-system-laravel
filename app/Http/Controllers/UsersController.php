dfa9ab8b95ccf1af340c40c2fd8e08b4be60cd5d
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-09-17 22:13:58 +0800

    update profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Users\UpdateProfileRequest;
 use App\Models\User;
 
 class UsersController extends Controller {
   public function index() {
 
-    return view('users.index')->with('users', User::all());
+    return view('users.index')->with('users', auth()->user());
+
+  }
+
+  public function edit() {
+    return view('users.edit')->with('user', auth()->user());
+  }
+
+  public function update(UpdateProfileRequest $request) {
+    $user = auth()->user();
+
+    $user->update([
+      'name'  => $request->name,
+      'about' => $request->about,
+
+    ]);
+
+    session()->flash('success', 'User udpated successfully.');
+    return redirect()->back();
+
   }
 
   public function makeAdmin(User $user) {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,12 +63,18 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+
+                                    <a class="dropdown-item" href="{{ route('users.edit-profile') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
